5ec02846c4c1400ffd8147eea61b83f7fecbc610
zouqilin <mr.zouqilin@gmail.com>
2017-06-16 23:06:50 +0800

    implement query_model

--- src/models/query_model.cpp
+++ src/models/query_model.cpp
@@ -1,9 +1,33 @@
 #include "query_model.h"
 
-QueryModel::QueryModel(QObject *parent) : QAbstractTableModel(parent)
+QueryModel::QueryModel(QSqlQuery *query, QObject *parent) : QAbstractTableModel(parent)
 {
+  this->query = query;
 }
 
 QueryModel::~QueryModel()
 {
 }
+
+QVariant QueryModel::data(const QModelIndex &index, int role) const {
+  if (!index.isValid())
+    return QVariant();
+
+  query->seek(index.row());
+  return query->value(index.column());
+}
+
+int QueryModel::columnCount(const QModelIndex &parent) const {
+  return query->record().count();
+}
+
+int QueryModel::rowCount(const QModelIndex &parent) const {
+  return query->numRowsAffected();
+}
+
+QVariant QueryModel::headerData(int section, Qt::Orientation orientation, int role) const {
+  if (role != Qt::DisplayRole)
+    return QVariant();
+  return query->record().value(section);
+}
+
--- src/models/query_model.h
+++ src/models/query_model.h
@@ -1,6 +1,12 @@
 #ifndef QUERY_MODEL_H
 
 #include <QAbstractTableModel>
+#include <QSqlRecord>
+#include <QSqlQuery>
+#include <QVariant>
+#include <QString>
+#include <QObject>
+#include <QModelIndex>
 
 #include "../utils/logger.h"
 
@@ -8,8 +14,24 @@ class QueryModel : public QAbstractTableModel {
   Q_OBJECT
 
   public:
-  explicit QueryModel(QObject *parent = Q_NULLPTR);
+  explicit QueryModel(QSqlQuery *query, QObject *parent = Q_NULLPTR);
   ~QueryModel();
+
+  public:
+  QSqlQuery *query;
+
+  public:
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+  int columnCount(const QModelIndex &parent = QModelIndex()) const;
+  int rowCount(const QModelIndex &parent = QModelIndex()) const;
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+  // setData();
+  // flags();
+  // insertRows();
+  // removeRows();
+  // insertColumns();
+  // removeColumns();
 };
 
 #endif // QUERY_MODEL_H
